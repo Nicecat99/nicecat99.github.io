2f5debfe48c93e04ed73c486bdd96525ea3933c1
Nicecat99 <94713118+Nicecat99@users.noreply.github.com>
2024-07-16 12:27:58 -0400

    test

--- resource/boss click/index.js
+++ resource/boss click/index.js
@@ -1,4 +1,6 @@
 // Edit html code
+import { tap, test } from './control.js';
+let i = 0;
 document.all[0].innerHTML = `
 <head>
 <link rel="stylesheet" href="style.css">
@@ -7,3 +9,13 @@ document.all[0].innerHTML = `
     <h1>Coming Soon...</h1>
 </body>
 `;
+
+test();
+function draw(){
+if(tap){
+i++;}
+document.body.innerHTML=i;
+requestAnimationFrame(draw);
+}
+
+draw();
