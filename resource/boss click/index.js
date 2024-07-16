2abcfc292a3af7a6f8be873c6524d0715918c4f2
Nicecat99 <94713118+Nicecat99@users.noreply.github.com>
2024-07-16 12:35:55 -0400

    Update index.js

--- resource/boss click/index.js
+++ resource/boss click/index.js
@@ -1,6 +1,9 @@
 // Edit html code
 import { keyPress, key, control } from './control.js';
-let i = 0;
+let player = {
+    x: innerWidth/2-150,
+    y: inner height/2-150,
+};
 document.all[0].innerHTML = `
 <head>
 <link rel="stylesheet" href="style.css">
