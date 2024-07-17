a1f36efd526eda6db010fe0f992fd3d6bdbcdc48
Nicecat99 <94713118+Nicecat99@users.noreply.github.com>
2024-07-17 15:26:09 -0400

    Update control.js

--- resource/boss click/control.js
+++ resource/boss click/control.js
@@ -1,6 +1,6 @@
 let keyPress = false;
 let key;
-function control(){
+function control(x){
 onkeydown = function(e){
     keyPress = true;
     key = e;
@@ -8,6 +8,21 @@ onkeydown = function(e){
 
 onkeyup = function(){
     keyPress = false;
+    if(key.key=='ArrowLeft' | key.key=='a'){
+        x.left=true;
+    }
+
+    if(key.key=='ArrowRight' | key.key=='d'){
+        x.right=true;
+    }
+
+    if(key.key=='ArrowUp' | key.key=='w'){
+        x.up=true;
+    }
+
+    if(key.key=='ArrowDown' | key.key=='s'){
+        x.down=true;
+    }
 };
 };
 
