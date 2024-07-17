3e9d3ed2440eeacb7f5f57ecf2b88a9802449285
Nicecat99 <94713118+Nicecat99@users.noreply.github.com>
2024-07-17 15:27:25 -0400

    Update index.js

--- resource/boss click/index.js
+++ resource/boss click/index.js
@@ -1,5 +1,11 @@
-// Edit html code
+// import code from other code
 import { keyPress, key, control } from './control.js';
+let move = {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+};
 let player = {
     x: innerWidth/2-150,
     y: innerHeight/2-150,
